569e9f2ec09f1352080c3d31b06566c0de63139e
nil.com <mateo.nihil@gmail.com>
2020-07-18 11:40:40 -0500

    Actualización 2

---
+++ trash/default.sql
@@ -0,0 +1,22 @@
+INSERT INTO `bio_plant`(
+    `nombre`,
+    `especie`,
+    `genero`,
+    `familia`,
+    `fecha`,
+    `habitat`,
+    `parte`,
+    `foto`,
+    `usuario`
+)
+VALUES(
+    'Helecho',
+    'Athyrium filix-femina',
+    'Pteridium',
+    'Pteridium aquilinum',
+    '2020-07-09',
+    'bosque tropical',
+    'hojas',
+    '../img/trash/plantasup/helecho.jpg',
+    'M'
+)
